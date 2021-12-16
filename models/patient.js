f83d2f7b2f077cf455bbe1ecae288bfc1c7b4cfd
Tom Highum <tomhighum@rocketmail.com>
2021-12-16 13:07:34 -0700

    patient and user models

---
+++ models/patient.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Patient extends Model {}
+
+Patient.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    CurrentMeds: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    primaryPhysician: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    pharmacy: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    
+    
+    },
+    {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'patient',
+  }
+);
