b18f217d85f76b53bbf1722d7bf0c7598f766dbf
Tom Highum <tomhighum@rocketmail.com>
2021-12-16 13:00:58 -0700

    created base model files

---
+++ models/event.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Event extends Model {}
+
+Event.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    temperture: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    temperatureTime: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    medication: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    medicationTime: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    medicationDosage: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    
+    
+    },
+    {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'events',
+  }
+);
