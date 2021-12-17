b69d971df144e64a76b9b97cce021d68a270e438
Matt Grogan <mattgrogan.dev@gmail.com>
2021-12-17 13:15:03 -0700

    login route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,1 +1,11 @@
+const router = require("express").Router();
 
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+module.exports = router;
