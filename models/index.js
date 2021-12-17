925aaf6a1afc181962e99bfc7e00fb23d9005b0a
Tom Highum <tomhighum@rocketmail.com>
2021-12-17 13:53:52 -0700

    updated index.js model with foreignkey setup

--- models/index.js
+++ models/index.js
@@ -1,3 +1,23 @@
 const User = require("./User");
+const Patient = require("./patient");
+const Event = require("./event")
 
-module.exports = { User };
+User.hasMany(Patient, {
+    foreignKey: "user_id",
+});
+
+Patient.belongsTo(User, {
+    foreignKey: "user_id",
+})
+
+Patient.hasMany(Event, {
+    foreignKey: "patient_id",
+});
+
+Event.belongsTo(Patient, {
+    foreignKey: "patient_id",
+})
+
+
+
+module.exports = { User, Patient, Event };
