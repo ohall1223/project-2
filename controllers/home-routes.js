cbec2bf30ce353351aed16670dc5a3e07e75f39e
wwbarton <wbartonw5@gmail.com>
2021-12-30 17:19:30 -0700

    hbs links

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,22 @@
+//home routes for all our gets!!!!!
+
 const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  // if (req.session.loggedIn) {
+  //   res.redirect("/");
+  //   return;
+  // }
+
+  res.render("landing");
+});
 
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect("/");
+    res.redirect("/journal");
     return;
   }
-  res.render("login");
-});
 
+  res.render("landing");
+});
 module.exports = router;
