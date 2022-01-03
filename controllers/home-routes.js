3613830ab9dec9a18473f4678f2fe1fc5b95491d
Olivia Hall <ohall1223@gmail.com>
2022-01-03 10:13:11 -0700

    routes worked

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 //home routes for all our gets!!!!!
 
 const router = require("express").Router();
+const withAuth = require("../utils/auth");
 
 router.get("/", (req, res) => {
   // if (req.session.loggedIn) {
@@ -11,12 +12,45 @@ router.get("/", (req, res) => {
   res.render("landing");
 });
 
-router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/journal");
+// router.get("/journal", withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findAll(req.session.user_id, {
+//       attributes: { exclude: ["password"] },
+//       include: [{ model: Project }],
+//     });
+
+//     const user = userData.get({ plain: true });
+
+//     res.render("journal", {
+//       ...user,
+//       logged_in: true,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+router.get("/journal", (req, res) => {
+  if (!req.session.loggedIn) {
+    res.redirect("/");
     return;
   }
 
-  res.render("landing");
+  res.render("journal");
+});
+
+router.get("/form", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("form");
+});
+
+router.get("/");
+router.get("/static", (req, res) => {
+  res.render("static");
 });
 module.exports = router;
